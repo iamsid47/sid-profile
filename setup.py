7833a697f763153480160b9f0e279dd7daf994a9
iamsid47 <siddheshkulthe43@gmail.com>
2023-03-30 14:06:06 +0530

    added url

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import codecs
 import os
 
 VERSION = '0.0.1'
-DESCRIPTION = 'Download Siddhesh Kulthe\'s Profile!'
-LONG_DESCRIPTION = 'A package that downloads Siddhesh Kulthe\'s Resume on your computer.'
+DESCRIPTION = 'Check out Siddhesh Kulthe\'s profile, or send him a direct message!'
+LONG_DESCRIPTION = 'A package that downloads Siddhesh Kulthe\'s Resume on your computer. All you need to do is to import siddhesh and use .Start() method to start the app!'
 
 # Setting up
 setup(
-    name="sid-profile",
+    name="siddhesh",
     version=VERSION,
     author="Siddhesh Kulthe",
     author_email="<siddheshkulthe43@gmail.com>",
@@ -16,10 +16,10 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[''],
-    keywords=['python', 'resume', 'sid', 'siddhesh kulthe', 'profile', 'CV'],
+    install_requires=['requests'],
+    keywords=['python', 'sid', 'siddhesh kulthe', 'profile', 'message', 'social', 'iamsid47'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
