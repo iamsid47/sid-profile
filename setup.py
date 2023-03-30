bcb948154c5cc20ff009c018f348b078c057b87c
iamsid47 <siddheshkulthe43@gmail.com>
2023-03-30 17:24:11 +0530

    updated version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 DESCRIPTION = 'Check out Siddhesh Kulthe\'s profile, or send him a direct message!'
 LONG_DESCRIPTION = 'A package that downloads Siddhesh Kulthe\'s Resume on your computer. All you need to do is to import siddhesh and use .Start() method to start the app!'
 
