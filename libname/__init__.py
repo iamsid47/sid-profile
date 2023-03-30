7d660cf491e296c8e2222409fb49d1794b414b88
iamsid47 <siddheshkulthe43@gmail.com>
2023-03-30 12:49:58 +0530

    updated app.py and init.py

--- libname/__init__.py
+++ libname/__init__.py
@@ -1,1 +1,11 @@
-from app import hello
+from app import Resume, Siddhesh
+import webbrowser
+
+URL = "https://linkedin.com/in/siddheshkulthe"
+
+print("Do you want to make a")
+choice = int(input("1. New Resume \n2. Check out Siddhesh\'s Resume?"))
+if choice == 1:
+    webbrowser.open(URL)
+if choice !=1:
+    Resume()
--- libname/app.py
+++ libname/app.py
@@ -1,2 +1,21 @@
-def hello ():
+
+social_list = []
+
+def Resume():
+    print("\nAmazing! Let's get your started :)")
+    name = str(input("Enter your name: "))
+    email = str(input("Enter your email: "))
+    phone = str(input("Enter your phone: "))
+    description = str(input("Enter your description: "))
+    choice2 = int(input("Do you want to add any social media links? (Y/N): "))
+    if choice2.lower() == "y":
+        print("\nGot it!")
+        linknumber = int(input("How many links do you want to enter? (Max 5): "))
+        for i in range(linknumber):
+            x = str(input("Enter your link number " + str(i+1) + ": "))
+
+
+def Siddhesh ():
     print("Hello World!")
+
+
